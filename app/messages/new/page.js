dbd6c6c9e715d8909697eec8add181e3f4f6ad98
jiyoungnim <cloundyon31@gmail.com>
2025-02-13 23:30:01 +0900

    📝 190. Setting Up Request Memoization For Custom Data Sources

--- app/messages/new/page.js
+++ app/messages/new/page.js
@@ -1,11 +1,11 @@
 import { redirect } from "next/navigation";
 
 import { addMessage } from "@/lib/messages";
-import { revalidatePath, revalidateTag } from "next/cache";
+// import { revalidatePath, revalidateTag } from "next/cache";
 
 export default function NewMessagePage() {
   async function createMessage(formData) {
-    ("use server");
+    "use server";
 
     const message = formData.get("message");
     addMessage(message);
@@ -13,7 +13,7 @@ export default function NewMessagePage() {
     // 189. On-Demand Cache Invalidation with revalidatePath & revalidateTag
     // ✨ messages 폴더의 page.js 파일에서 설정하는 방식(revalidate/dynamic/unstable_noStore)과 달리,
     // 컴포넌트 내부에서 revalidate 관련 훅을 직접 호출하여 더 세밀하고 성능적으로 효율적인 캐시 무효화 기능을 활용!
-    revalidateTag("msg"); // 'msg' 태그가 달린 캐시 데이터를 무효화하여 최신 메시지를 가져오도록 함
+    // revalidateTag("msg"); // 'msg' 태그가 달린 캐시 데이터를 무효화하여 최신 메시지를 가져오도록 함
 
     // revalidatePath("/messages"); // '/messages' 페이지의 캐시를 무효화하여 메시지 목록을 최신 상태로 갱신함
     // revalidatePath("/", "layout"); // 루트 레이아웃을 다시 렌더링하여 최신 상태를 반영하도록 함
--- app/messages/page.js
+++ app/messages/page.js
@@ -1,6 +1,6 @@
-import { unstable_noStore } from "next/cache";
-
+// import { unstable_noStore } from "next/cache";
 import Messages from "@/components/messages";
+import { getMessages } from "@/lib/messages";
 
 // 1️⃣ revalidate 상수를 export하여 파일 내 모든 컴포넌트의 데이터 캐싱 주기를 설정
 // ✅ 이 파일 내의 모든 fetch 요청에 대해 5초마다 데이터를 재검증(revalidate)
@@ -14,10 +14,12 @@ export default async function MessagesPage() {
   // 3️⃣ unstable_noStore() 함수를 사용하여 특정 컴포넌트에서만 캐싱을 비활성화
   // ✅ 파일 전체가 아닌 이 MessagesPage 컴포넌트에서만 캐싱이 비활성화되는 효과
   // unstable_noStore();
-  const response = await fetch("http://localhost:8080/messages", {
-    next: { tags: ["msg"] },
-  });
-  const messages = await response.json();
+
+  // const response = await fetch("http://localhost:8080/messages", {
+  //   next: { tags: ["msg"] },
+  // });
+  // const messages = await response.json();
+  const messages = getMessages();
 
   if (!messages || messages.length === 0) {
     return <p>No messages found</p>;
